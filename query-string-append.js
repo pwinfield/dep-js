2f673029d847dfa2e75468eb67a6883f2d6b3cc7
Pete Winfield <pwinfield@cloudbees.com>
2025-06-23 13:50:17 -0700

    Another attempt at the MutationObserver

--- query-string-append.js
+++ query-string-append.js
@@ -2,39 +2,35 @@ const mainContainer = document.getElementById('main');
 
 if (mainContainer) {
   const observer = new MutationObserver((mutationsList, observer) => {
-    if (mainContainer.querySelector('#67d21fe151ed7')) {
+    const link = mainContainer.querySelector('a[href*="https://www.cloudbees.com/contact-us"]');
+    if (link) {
       observer.disconnect();
       console.log('Ceros Experience loaded!');
-      queryStringAppendFunction();
+      queryStringAppendFunction(link);
     }
   });
 
   observer.observe(mainContainer, { childList: true, subtree: true });
 }
 
-function queryStringAppendFunction() {
-  const link = document.getElementById('67d21fe151ed7');
-
+function queryStringAppendFunction(link) {
   console.log('Link found:', link);
 
-  if (link) {
-    link.addEventListener('click', function (event) {
-      event.preventDefault(); // Prevent the default link navigation
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
 
-      const url = new URL(link.href);
+    const url = new URL(link.href);
+    const queryString = window.location.search;
 
-      const queryString = window.location.search;
-      if (queryString) {
-        const currentParams = new URLSearchParams(queryString);
-        currentParams.forEach((value, key) => {
-          url.searchParams.set(key, value); // Merge or override query params
-        });
-      }
+    if (queryString) {
+      const currentParams = new URLSearchParams(queryString);
+      currentParams.forEach((value, key) => {
+        url.searchParams.set(key, value);
+      });
+    }
 
-      console.log('Redirecting to:', url.toString());
-      window.location.href = url.toString(); // Manually navigate
-    });
-  } else {
-    console.warn('Link not found');
-  }
+    console.log('Redirecting to:', url.toString());
+    window.location.href = url.toString();
+  });
+}
 }
